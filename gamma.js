785a79142990544b5563d31ee1f1960c2cd59b1f
OneShot2014 <115625587+OneShot2014@users.noreply.github.com>
2022-10-12 17:45:42 +0300

    Update gamma.js

--- gamma.js
+++ gamma.js
@@ -16,14 +16,7 @@ var context = canvas.getContext("2d");
 var centru = {x:canvas.width/2, y:canvas.height/2}
 var latura_patrat = 50;
 
-context.setTransform(1, 0, 0, 1, 0, 0);
-context.clearRect(0,0,canvas.width, canvas.height);
-context.translate(centru.x/2, centru.y/2);
-context.rotate(alpha * Math.PI / 180);
-context.beginPath();
-context.rect(-latura_patrat,-latura_patrat, latura_patrat, latura_patrat);
-context.stroke();
-}
+
 
 context.clearRect(0, 0, canvas.width, canvas.height);
 context.fillStyle = "#6A0888";
